16b6ff789c49c0254fce4fb17189a493d40acdac
hagarSamy <hs1800@fayoum.edu.eg>
2024-02-28 19:00:22 +0200

    deleting a binary tree

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -8,17 +8,17 @@
 
 void binary_tree_delete(binary_tree_t *tree)
 {
-    if (tree == NULL)
-        return;
+	if (tree == NULL)
+		return;
 
-    binary_tree_delete(tree->left);
-    binary_tree_delete(tree->right);
-    if (tree->parent != NULL)
-    {
-        if (tree->parent->left == tree)
-            tree->parent->left = NULL;
-        else if (tree->parent->right == tree)
-            tree->parent->right = NULL;
-    }
-    free(tree);
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
+	if (tree->parent != NULL)
+	{
+		if (tree->parent->left == tree)
+			tree->parent->left = NULL;
+		else if (tree->parent->right == tree)
+			tree->parent->right = NULL;
+	}
+	free(tree);
 }
