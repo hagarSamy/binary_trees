21f53e5266c8718b8a16780af72fa0947225ff79
hagarSamy <hs1800@fayoum.edu.eg>
2024-02-28 10:58:07 +0200

    inserting at left

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -11,8 +11,10 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *newNode;
 
+	if (parent == NULL)
+		return (NULL);
 	newNode = malloc(sizeof(binary_tree_t));
-	if ((newNode == NULL) || (parent == NULL))
+	if ((newNode == NULL))
 		return (NULL);
 	newNode->parent = parent;
 	newNode->right = NULL;
