aad9bf42f91f37ea73d4e1a89b08bf13d3acc44f
HagarSamy <hagarsami63@gmai.com>
2024-02-29 16:12:02 +0200

    finding the height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,12 +7,11 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height, right_height;
-	size_t one = 1;
+	int left_height, right_height;
 
 	if (tree == NULL)
 		return (-1);
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
-	return (((left_height > right_height) ? left_height : right_height) + one);
+	return (((left_height > right_height) ? left_height : right_height) );
 }
