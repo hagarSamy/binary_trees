4b5cf215f9281c28556e2b20a552a2bf7efec5cf
hagarSamy <hs1800@fayoum.edu.eg>
2024-02-29 23:59:14 +0200

    measuring balance factor

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -12,27 +12,27 @@ int binary_tree_balance(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	left_height = height(tree->left);
-	right_height = height(tree->right);
+	left_height = length(tree->left);
+	right_height = length(tree->right);
 	return (left_height - right_height);
 }
 
 /**
- * height - gets height of a node
- * @parent: node to get its height
- * Return: height of node or 0 if it's null
+ * length - gets length of a node
+ * @parent: node to get its length
+ * Return: length of node or 0 if it's null
 */
 
-int height(binary_tree_t *parent)
+int length(binary_tree_t *parent)
 {
-	int lheight, rheight;
+	int llength, rlength;
 
 	if (parent == NULL)
 		return (0);
-	lheight = height(parent->left);
-	rheight = height(parent->right);
-	if (lheight > rheight)
-		return (lheight + 1);
+	llength = length(parent->left);
+	rlength = length(parent->right);
+	if (llength > rlength)
+		return (llength + 1);
 	else
-		return (rheight + 1);
+		return (rlength + 1);
 }
